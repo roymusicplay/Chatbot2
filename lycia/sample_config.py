09b2f17b69d2ad016fc32484aa228ebdf68d4327
ABHINAS ROY <85425565+roymusicplay@users.noreply.github.com>
2021-08-08 09:02:54 +0530

    Update sample_config.py

--- lycia/sample_config.py
+++ lycia/sample_config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int("api_id")
-    API_HASH = "api_hash"
-    TOKEN = "token"
+    API_ID = int("7392392")
+    API_HASH = "ff8ee60e4b9f66552ee0546e47c5565b"
+    TOKEN = "1915403187:AAGjs67rmfCugZ9fvLwXsSs3Hzcrv6qKuck"
